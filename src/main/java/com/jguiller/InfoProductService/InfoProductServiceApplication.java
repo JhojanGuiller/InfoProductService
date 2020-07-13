559e0521e10f6f5996c42071d579b56b1a5d7749
JhojanGuille <guille.balvin@gmail.com>
2020-07-13 10:07:50 -0500

    Estructura inicial del microservicio

---
+++ src/main/java/com/jguiller/InfoProductService/InfoProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jguiller.InfoProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoProductServiceApplication.class, args);
+	}
+
+}
